0a608a53357b35ba7296083d60041d0426f27620
wangzin <wangzin53@gmail.com>
2021-05-30 19:29:43 +0600

    changes in the organizaiton

---
+++ emis-ui/resources/js/components/organization/general/section/list_section.vue
@@ -0,0 +1,77 @@
+<template>
+    <div>
+        <table id="sectiont-table" class="table table-bordered text-sm table-striped">
+            <thead>
+                <tr>
+                    <th>SL#</th>
+                    <th>Class</th>
+                    <th>Stream</th>
+                    <th>Section</th>
+                    <th>Action</th> 
+                </tr>
+            </thead>
+            <tbody id="tbody">
+                <tr v-for="(item, index) in data_list" :key="index">
+                    <td>{{ index + 1 }}</td>
+                    <td>{{ item.class}}</td>
+                    <td>{{ item.stream}}</td>
+                    <td>{{ item.section}}</td>
+                    <td>
+                        <div class="btn-group btn-group-sm">
+                            <a href="#" class="btn btn-info" @click="viewEquipmentList(item)"><i class="fas fa-edit"></i ></a>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            data_list:[],
+        }
+    },
+    methods:{
+
+        /**
+         * method ti load equipment list
+         */
+        loadEquipmentList(uri = 'organization/loadEquipment/null'){
+            axios.get(uri)
+            .then(response => {
+                let data = response;
+                this.data_list =  data.data;
+            })
+            .catch(function (error) {
+                if(error.toString().includes("500")){
+                    $('#tbody').html('<tr><td colspan="6" class="text-center text-danger text-bold">This server down. Please try later</td></tr>');
+                }
+            });
+        },
+
+        /**
+         * method to view equipment list
+         */
+        viewEquipmentList(data){
+            data.action='edit';
+            this.$router.push({name:'EquipmentEdit',params: {data:data}});
+        },
+    },
+
+    mounted(){
+        this.loadEquipmentList();
+        this.dt =  $("#sectiont-table").DataTable()
+    },
+    watch: {
+        data_list(){
+            this.dt.destroy();
+            this.$nextTick(() => {
+                this.dt =  $("#sectiont-table").DataTable()
+            });
+        }
+    },
+}
+</script>
--- emis-ui/resources/js/components/organization/general/section/section_index.vue
+++ emis-ui/resources/js/components/organization/general/section/section_index.vue
@@ -1,14 +1,21 @@
 <template>
     <div>
-        <div class="card card-primary card-outline">
-            <div class="card-header pt-0 mt-0 pb-0"> 
-                <span class="fa-pull-right pr-2 py-1">
-                    <button type="button" class="btn btn-primary text-white btn-sm" @click="showadprocess('section_list')"><i class="fa fa-list"></i> List</button>
-                    <button type="button" class="btn btn-dark text-white btn-sm" @click="showadprocess('section_add')"><i class="fa fa-plus"></i> Add New </button>
-                </span>
-            </div>
-            <div class="card-body pt-1 pb-0">  
-                <router-view></router-view> 
+        <div class="callout callout-danger" style="display:none" id="screenPermission">
+            <h5 class="bg-gradient-danger">Sorry!</h5>
+            <div id="message"></div>
+        </div>
+        <div id="mainformid">
+            <div class="card card-primary card-outline">
+                <div class="card-header pt-0 mt-0 pb-0"> 
+                    <label class="fa-pull-left">Section</label>
+                    <span class="fa-pull-right pr-2 py-1">
+                        <button type="button" class="btn btn-primary text-white btn-sm" @click="showadprocess('list_section')"><i class="fa fa-list"></i> List</button>
+                        <button type="button" class="btn btn-dark text-white btn-sm" @click="showadprocess('create_section')"><i class="fa fa-plus"></i> Add New </button>
+                    </span>
+                </div>
+                <div class="card-body pt-1 pb-0">  
+                    <router-view></router-view> 
+                </div>
             </div>
         </div>
     </div>
@@ -17,13 +24,26 @@
 export default {
     methods: {
         showadprocess(type){
-            if(type=="section_add" || type=="section_edit"){
-                this.$router.push("/"+type);
-            }
-            if(type=="section_list"){
-                this.$router.push("/section_list");
-            }
+            this.$router.push({name:type});
 		},
     },
+    mounted(){
+        axios.get('common/getSessionDetail')
+        .then(response => {
+            let data = response.data.data;
+            if(data['acess_level']=="Org"){
+                this.form.school=data['Agency_Code'];
+                this.getCurrentClassStream(data['Agency_Code']);
+            }
+            else{
+                $('#screenPermission').show();
+                $('#mainformid').hide();
+                $('#message').html('This page is not accessible to you');
+            }
+        })    
+        .catch(errors => { 
+            console.log(errors)
+        });
+    }
 }
 </script>
