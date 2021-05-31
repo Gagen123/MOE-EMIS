22acb297889aa45ca1f49742c523d1449d14f9b4
Gagen123 <gagendraghalley@newedge.bt>
2021-05-31 09:36:03 +0600

    some changes

--- emis-portal/app/Http/Controllers/NotenrolledController.php
+++ emis-portal/app/Http/Controllers/NotenrolledController.php
@@ -268,7 +268,6 @@ class NotenrolledController extends Controller
     }
 
     public function getstudentdetailsbyCid($cid=""){
-        // dd("dsdsad");
         $response_data= $this->apiService->listData('emis/students/admission/getstudentdetailsbyCid/'.$cid);
         return $response_data;
     }
