63a5344a27643c416a65052054945a1679d2b4c1
phuntshoemis <phuntsho.ugyel@newedge.bt>
2021-06-08 07:25:33 +0600

    last minute

--- emis-ui/resources/js/components/organization/view_organization_profile.vue
+++ emis-ui/resources/js/components/organization/view_organization_profile.vue
@@ -30,10 +30,6 @@
                                 <li class="nav-item border-left border-right">
                                     <a class="nav-link" id="custom-tabs-four-messages-tab" data-toggle="pill" href="#custom-tabs-four-messages" role="tab" aria-controls="custom-tabs-four-messages" aria-selected="false">Staff & Student Information</a>
                                 </li>
-                                <li class="nav-item border-left border-right">
-                                    <a class="nav-link" id="custom-tabs-four-settings-tab" data-toggle="pill" href="#custom-tabs-four-settings" role="tab" aria-controls="custom-tabs-four-settings" aria-selected="false">Organizational Facilities</a>
-                                </li>
-                                
                             </ul>
                         </div>
                         <div class="card-body">
@@ -203,61 +199,7 @@
                                     </table>
                                 </div>
                             </div>
-                            <div class="tab-pane fade" id="custom-tabs-four-settings" role="tabpanel" aria-labelledby="custom-tabs-four-settings-tab">
-                                <div class="card-body p-0">
-                                    <table class="table table-sm">
-                                    <thead>
-                                        <tr>
-                                            <th style="width: 10px">#</th>
-                                            <th>Type</th>
-                                            <th>Status</th>
-                                        </tr>
-                                    </thead>
-                                    <!-- <tbody>
-                                        <tr>
-                                        <td>1.</td>
-                                        <td>Update software</td>
-                                        <td>
-                                            <div class="progress progress-xs">
-                                            <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
-                                            </div>
-                                        </td>
-                                        <td><span class="badge bg-danger">55%</span></td>
-                                        </tr>
-                                        <tr>
-                                        <td>2.</td>
-                                        <td>Clean database</td>
-                                        <td>
-                                            <div class="progress progress-xs">
-                                            <div class="progress-bar bg-warning" style="width: 70%"></div>
-                                            </div>
-                                        </td>
-                                        <td><span class="badge bg-warning">70%</span></td>
-                                        </tr>
-                                        <tr>
-                                        <td>3.</td>
-                                        <td>Cron job running</td>
-                                        <td>
-                                            <div class="progress progress-xs progress-striped active">
-                                            <div class="progress-bar bg-primary" style="width: 30%"></div>
-                                            </div>
-                                        </td>
-                                        <td><span class="badge bg-primary">30%</span></td>
-                                        </tr>
-                                        <tr>
-                                        <td>4.</td>
-                                        <td>Fix and squish bugs</td>
-                                        <td>
-                                            <div class="progress progress-xs progress-striped active">
-                                            <div class="progress-bar bg-success" style="width: 90%"></div>
-                                            </div>
-                                        </td>
-                                        <td><span class="badge bg-success">90%</span></td>
-                                        </tr>
-                                    </tbody> -->
-                                    </table>
-                                </div>
-                            </div>
+                            
                             </div>
                         </div>
                         <!-- /.card -->
