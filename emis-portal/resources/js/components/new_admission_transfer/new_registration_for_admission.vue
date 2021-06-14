92c84a548577427cc7853f4b7808b79f3fbc18a0
wangzin <wangzin53@gmail.com>
2021-06-14 06:40:08 +0600

    pulling changes

--- emis-portal/resources/js/components/new_admission_transfer/new_registration_for_admission.vue
+++ emis-portal/resources/js/components/new_admission_transfer/new_registration_for_admission.vue
@@ -860,11 +860,59 @@ export default {
                 dzoId=id;
             }
             this.s_schoolList = [];
-            axios.get('/masters/getOrgList/'+dzoId+'/'+type)
-              .then(Response =>{
-                let data = Response.data.data;
-                this.s_schoolList = data;
-              })
+            let date2=new Date($('#dob').val());
+            let year2=date2.getFullYear();
+            let month2=date2.getMonth();
+            let numberOfMonths;
+            let isvalid=true;
+            let messages="";
+            alert(type+this.ppmonth);
+            if(type=="ECCD"){
+                let date1=new Date(this.eccddob);
+                let year1=date1.getFullYear();
+                let month1=date1.getMonth();
+                if(month1===0){ //Have to take into account
+                    month1++;
+                    month2++;
+                }
+                //numberOfMonths = (year2 - year1) * 12 + (month2 - month1) - 1;//excluding both month1 and month2
+                numberOfMonths = (year2 - year1) * 12 + (month2 - month1);//include either of the months
+                //numberOfMonths = (year2 - year1) * 12 + (month2 - month1)+1;//include both of the months
+                if(this.eccdmonth>numberOfMonths){
+                    isvalid=false;
+                }
+                messages="From your DOB, you are just "+numberOfMonths+' months old. You are eligible to register only at the total month of '+this.eccdmonth;
+            }
+            else{
+                let date1=new Date(this.ppdob);
+                let year1=date1.getFullYear();
+                let month1=date1.getMonth();
+                if(month1===0){ //Have to take into account
+                    month1++;
+                    month2++;
+                }
+               // numberOfMonths = (year2 - year1) * 12 + (month2 - month1) - 1;//excluding both month1 and month2
+                numberOfMonths = (year2 - year1) * 12 + (month2 - month1);//include either of the months
+                //numberOfMonths = (year2 - year1) * 12 + (month2 - month1)+1;//include both of the months
+                if(this.ppmonth> numberOfMonths){
+                    isvalid=false;
+                }
+                messages="From your DOB, you are just "+numberOfMonths+' months old. You are eligible to register only at the total month of '+this.ppmonth;
+            }
+            if(isvalid==false){
+                Swal.fire(
+                    'Sorry',
+                    messages,
+                    'error',
+                );
+            }
+            else{
+                axios.get('/masters/getOrgList/'+dzoId+'/'+type)
+                .then(Response =>{
+                    let data = Response.data.data;
+                    this.s_schoolList = data;
+                })
+            }
           },
          getclassList(id){
             let orgId=$('#school').val();
