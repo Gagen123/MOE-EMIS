3c7dde4e2c668ed32e3c9ae08ef1daf18c3890f3
sarulimbu504 <saru.limboo@newedge.bt>
2021-06-23 09:15:44 +0600

    files downlaod

--- emis-ui/resources/js/components/organization/structuralFacility/structural_index.vue
+++ emis-ui/resources/js/components/organization/structuralFacility/structural_index.vue
@@ -60,12 +60,12 @@
                         Income Facilities
                     </router-link>
                 </li>
-                <li class="nav-item active pr-1">
+                <!-- <li class="nav-item active pr-1">
                     <router-link id="disasters_information" to="/disasters_information" class="btn btn-outline-primary btn-sm pb-0 pl-1 pr-1 pt-0">
                         <span class=""></span>
                         Disaster Information
                     </router-link>
-                </li>
+                </li> -->
                 <li class="nav-item active pr-1">
                     <router-link id="compound_information" to="/compound_information" class="btn btn-outline-primary btn-sm pb-0 pl-1 pr-1 pt-0">
                         <span class=""></span>
