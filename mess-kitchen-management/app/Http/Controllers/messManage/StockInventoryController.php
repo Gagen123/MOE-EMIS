a0f84717b709dd5fc8cdb0442f70b552fa1dfeff
sarulimbu504 <saru.limboo@newedge.bt>
2021-07-12 11:02:35 +0600

    files download

--- mess-kitchen-management/app/Http/Controllers/messManage/StockInventoryController.php
+++ mess-kitchen-management/app/Http/Controllers/messManage/StockInventoryController.php
@@ -32,16 +32,22 @@ class StockInventoryController extends Controller
         //   // $response_data=StockReceived::where('organizationId',$org_id)->get();
         //   // return $this->successResponse($response_data);
      //  try{
-            $response_data = DB::select("SELECT a.item_id, a.organizationId, a.unit_id, a.stock_received_quantity, b.stock_issed_quantity, c.local_procured_quantity
-          FROM
-          (SELECT a.item_id, f.organizationId, a.unit_id, SUM(a.receivedquantity) AS stock_received_quantity FROM
-           stock_received_items a
-           LEFT JOIN stock_receiveds f ON a.stockreceivedId = f.id GROUP BY f.organizationId, a.item_id, a.unit_id) a
-          LEFT JOIN
-          (SELECT item_id,organizationId, unit_id, SUM(quantity) AS stock_issed_quantity FROM stock_issueds GROUP BY organizationId, item_id, unit_id) b ON a.item_id=b.item_id
-          LEFT JOIN
-          (SELECT item_id,organizationId, unit_id, SUM(quantity) AS local_procured_quantity FROM local_procures GROUP BY organizationId, item_id, unit_id) c ON a.item_id=c.item_id
-         WHERE a.organizationId = ? GROUP BY a.stock_received_quantity,  b.stock_issed_quantity, c.local_procured_quantity, a.organizationId, a.unit_id, a.item_id",[$orgId]);
+            $response_data = DB::select("SELECT aa.item_id, aa.organizationId, aa.unit_id, aa.stock_received_quantity, b.stock_issed_quantity, aa.local_procured_quantity
+            FROM
+            (SELECT  c.item_id, c.organizationId, c.unit_id, SUM(c.stock_received_quantity) AS stock_received_quantity , 
+            SUM(c.local_procured_quantity) AS local_procured_quantity
+            FROM
+            (SELECT a.item_id, f.organizationId, a.unit_id, a.receivedquantity AS stock_received_quantity , 0 AS local_procured_quantity
+             FROM stock_received_items a 
+             LEFT JOIN stock_receiveds f ON a.stockreceivedId = f.id
+             UNION
+             SELECT item_id,organizationId, unit_id, 0 AS stock_received_quantity, quantity AS local_procured_quantity 
+             FROM local_procures) c 
+        GROUP BY c.item_id, c.organizationId, c.unit_id) aa
+            LEFT JOIN
+            (SELECT item_id,organizationId, unit_id, SUM(quantity) AS stock_issed_quantity 
+          FROM stock_issueds GROUP BY organizationId, item_id, unit_id) b ON aa.item_id=b.item_id
+         WHERE a.organizationId = ? GROUP BY aa.stock_received_quantity,  b.stock_issed_quantity, aa.local_procured_quantity, aa.organizationId, aa.unit_id, aa.item_id",[$orgId]);
          return $this->successResponse($response_data);
     //    }catch(Exception $e){
     //     dd($e);
