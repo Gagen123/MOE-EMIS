0d9ff22d9d85c0f4eac4b08279fb5163950c12aa
Gagen123 <Gagendra.ghalley@newedge.bt>
2021-07-30 18:14:20 +0600

    dsadsad

--- emis-ui/resources/js/components/staff/transfer/inter_transfer/edit_inter_transfer.vue
+++ emis-ui/resources/js/components/staff/transfer/inter_transfer/edit_inter_transfer.vue
@@ -193,6 +193,12 @@
                                 </table>
                             </div>
                         </div>
+                        <div class="form-group row" id="Withdraw">
+                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                <label><i class="text-danger">(If you wish to cancel the transfer after processing)</i></label><br/>
+                                <label class="mb-1">Withdraw: <input type="checkbox" name="withdraw" v-model="form.withdraw" id="withdraw" class="icheck-success d-inline"></label>
+                            </div>
+                        </div>
                         <hr>
                         <div  class="row form-group fa-pull-right">
                          <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" id="tbName">
@@ -241,6 +247,7 @@ export default {
                 schoolName:'',
                 status:'',
                 remarks:'',
+                withdraw:'',
                 dzongkhagApproved:'',
                 application_number:'',
                 preference_school:'',
@@ -419,7 +426,8 @@ export default {
                             }
                             let formData = new FormData();
                             formData.append('id', this.form.id);
-                            formData.append('reason_id', this.form.reason_id);
+                            formData.append('withdraw', this.form.withdraw);
+                             formData.append('reason_id', this.form.reason_id);
                             formData.append('description', this.form.description);
                             formData.append('application_number', this.form.application_number);
                             formData.append('preference_dzongkhag1', this.form.preference_dzongkhag1);
