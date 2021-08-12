e1b5d78b9ebd08277765f4c66b139f295ced843b
wangzin <wangzin53@gmail.com>
2021-08-12 12:30:44 +0600

    pulling changes

--- emis-ui/resources/js/components/projection_indicator/student_learners/student_learner_index.vue
+++ emis-ui/resources/js/components/projection_indicator/student_learners/student_learner_index.vue
@@ -1,18 +1,18 @@
 <template>
 <div>
     <ol class="mb-1 ml-xl-n4 mr-xl-n2" style="background-color:#E5E5E5">
-        <li class="form-inline "><h5 class="pt-1">Student/Learner/Children</h5></li>
+        <li class="form-inline "><h5 class="pt-1">Student/Learner/Children Information</h5></li>
     </ol>
     <div class="container-fluid">
         <ul class="nav nav-pills mb-3" id="mainmenu" role="tablist">
             <li class="nav-item pr-1">
-                <router-link to="/eccd_children_index" class="btn btn-outline-primary btn-sm pb-0 pl-1 pr-1 pt-0">
+                <router-link to="/student_index" class="btn btn-outline-primary btn-sm pb-0 pl-1 pr-1 pt-0">
                    <span class=""></span>
-                   Students
+                   Student
                 </router-link>
             </li>
             <li class="nav-item pr-1">
-                <router-link to="/eccd_children_index" class="btn btn-outline-primary btn-sm pb-0 pl-1 pr-1 pt-0">
+                <router-link to="/student_enrolment" class="btn btn-outline-primary btn-sm pb-0 pl-1 pr-1 pt-0">
                    <span class=""></span>
                    Enrollment
                 </router-link>
@@ -33,25 +33,6 @@
                 <router-link to="/eccd_children_index" class="btn btn-outline-primary btn-sm pb-0 pl-1 pr-1 pt-0">
                     <span class=""></span>
                     Transition Rate
-                </router-link>
-            </li>
-
-            <li class="nav-item pr-1">
-                <router-link to="/eccd_children_index" class="btn btn-outline-primary btn-sm pb-0 pl-1 pr-1 pt-0">
-                   <span class=""></span>
-                   Eccd
-                </router-link>
-            </li>
-            <li class="nav-item pr-1">
-                <router-link to="/school_student_index" class="btn btn-outline-primary btn-sm pb-0 pl-1 pr-1 pt-0">
-                   <span class=""></span>
-                    School
-                </router-link>
-            </li>
-            <li class="nav-item pr-1">
-                <router-link to="/tertiary_learners_index" class="btn btn-outline-primary btn-sm pb-0 pl-1 pr-1 pt-0">
-                   <span class=""></span>
-                    Tertiary
                 </router-link>
             </li>
         </ul>
