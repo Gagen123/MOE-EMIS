8e28ff29fcd10699413f57de01aa16e3b91a1137
wangzin <wangzin53@gmail.com>
2021-10-13 11:42:05 +0600

    changes in staff

--- emis-ui/resources/js/components/staff/private/create_private_staff.vue
+++ emis-ui/resources/js/components/staff/private/create_private_staff.vue
@@ -1099,12 +1099,12 @@ export default {
                                     let personal_detail = response.data;
                                     this.personal_form.name = personal_detail.firstName + " " + personal_detail.lastName;
                                     let dob=personal_detail.dob;
-                                    if(dob.includes('-')){
-                                        dob=dob.split('-')[2]+'-'+dob.split('-')[1]+'-'+dob.split('-')[0];
-                                    }
-                                    if(dob.includes('/')){
-                                        dob=dob.split('/')[2]+'-'+dob.split('/')[1]+'-'+dob.split('/')[0];
-                                    }
+                                    // if(dob.includes('-')){
+                                    //     dob=dob.split('-')[2]+'-'+dob.split('-')[1]+'-'+dob.split('-')[0];
+                                    // }
+                                    // if(dob.includes('/')){
+                                    //     dob=dob.split('/')[2]+'-'+dob.split('/')[1]+'-'+dob.split('/')[0];
+                                    // }
                                     this.personal_form.dob = dob;
                                     $('#dob').val(dob);
                                     if(personal_detail.gender=="M"){
