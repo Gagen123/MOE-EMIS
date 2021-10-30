05f93b1afa7d305ea1c08df31919b0a261ce617c
Chimi-S <chimithinley705@gmail.com>
2021-10-30 15:52:19 +0600

    Changes

--- emis-ui/resources/js/components/spms/spms_index.vue
+++ emis-ui/resources/js/components/spms/spms_index.vue
@@ -12,7 +12,7 @@
                     </router-link>
                 </li>
             </ul>
-            <ul class="nav nav-pills mb-2" id="mainmenu" role="tablist">
+            <ul class="nav nav-pills mb-2 developemntEnv" id="mainmenu" role="tablist">
                 <!-- <li class="nav-item active pr-1"  v-for="(item, index) in menubar" :key="index">
                     <router-link :to="{name: item.route, query: {data: item.actions } }" class="btn btn-outline-primary btn-sm pb-0 pl-1 pr-1 pt-0"  onclick="afterclick()">
                         <span :class="item.screen_icon"></span> 
